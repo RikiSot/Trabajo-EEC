2d983492ccd5d80b95bf054ffe3940129b926636
Riki <rkgomezz@gmail.com>
2020-12-26 19:51:42 +0100

    adc en aAN1. comentarios para cambiar

--- adc.c
+++ adc.c
@@ -27,8 +27,8 @@ void adcinit(void) //por comprobar en la placa
 
   ADC1CON2 = 0b0000000000000000; // voltage reference en ADC1CON2
   ADC1CON3 = 0b1000000000000000; // select the analog conversion clock <7:0> y <15> para enable Tad
-//  AD1PCFGL = 0b1111111111101111; // AN4 entrada analogica - RB1 - PGC en placa
-//  AD1CHS0  = 0b0000010000000100; // AN4 entrada seleccionada para el CH0
+//  AD1PCFGL = 0b1111111111111110; // AN1 entrada analogica - RB1 - PGC en placa
+//  AD1CHS0  = 0b0000000100000001; // AN1 entrada seleccionada para el CH0
   AD1PCFGL = 0b0000000011110111; // AN4 entrada analogica - RB1 - PGC en placa
   AD1CHS0  = 0b0000010000000100; // AN4 entrada seleccionada para el CH0
 
