3452874260a1819eb18d73c9e28dd2590446445a
Riki <rkgomezz@gmail.com>
2021-01-25 18:01:36 +0100

    cambios bluetooth

--- BLUETOOTH.h
+++ BLUETOOTH.h
@@ -6,6 +6,7 @@
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void init_BT(void); //inits and enables uart isr
 void enviar_datos(unsigned int16 ppm);
+
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void init_BT(void)
 {
@@ -13,18 +14,17 @@ void init_BT(void)
   	fprintf(BT,"SF,1\r");      delay_ms(500); // factory defaults
   	fprintf(BT,"SA,0\r");      delay_ms(500); // authentication 'Open'
   	fprintf(BT,"R,1\r");       delay_ms(500); // Run device
+
+    fprintf(BT,"$Representacion grafica,Funcion,datos 1\n");
+    fprintf(BT,"&0,1000,0,1,0\n");
+    fprintf(BT,"#0,200,0,1,0\n");
 }
 
 void enviar_datos(unsigned int16 ppm)
 {
 	COM:
 	while(!input(COMM));
-		fprintf(BT,"$Representacion grafica,Funcion,datos 1\n");
- 		fprintf(BT,"&0,1000,0,1,0\n");
- 		fprintf(BT,"#0,200,0,1,0\n");
-
  		fprintf(BT, "%03d\n", ppm);
-		delay_ms(4);
 		if(!input(COMM))
 		goto COM;
 }
